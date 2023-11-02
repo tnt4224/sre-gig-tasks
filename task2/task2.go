b011c964609ac4d7ffb107bb559efaf73a4ff014
No Email <no@email.com>
2023-11-02 14:13:54 -0500

    updated conditional so that if the random number is even and higher than 50, print "It's closer to 100, and it's even!"

--- task2/task2.go
+++ task2/task2.go
@@ -16,6 +16,10 @@ func main() {
         // iii. Print the generated random number
 	//
 	// 2. Modify the previous code to print "It's 50!" if the random number is 50
+	// 
+	// 3. Modify the conditional in the code you previously wrote to check not only 
+	//    if the  number is higher than 50, but also if it's even. If it's even and 
+	//    higher than 50, print,"It's closer to 100, and it's even!"
 
 	// NOTE: 
 	// rand.Seed has been deprecated since v 1.20 
@@ -24,7 +28,9 @@ func main() {
 	//rand.Seed(time.Now().UnixNano())
 	var number = genRandomInt(1, 100)
 
-	if number > 50 {
+	if number > 50 && (number % 2 == 0) {
+		fmt.Println("It's closer to 100, and it's even!")
+	} else if number > 50 {
 		fmt.Println("It's closer to 100")
 	} else if number < 50 {
 		fmt.Println("It's closer to 0")
