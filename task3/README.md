512929ae22393faff1c9d003c9ff9b55441b11a3
No Email <no@email.com>
2023-11-07 15:14:12 -0600

    added templates and css for each news item

---
+++ task3/README.md
@@ -0,0 +1,6 @@
+
+## Task 3  (Async & To be reviewed during stand-ups)
+
+1. Build a simple web server in go. Use your creativity to enhance it.
+
+
---
+++ task3/task3.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"io"
+	"html/template"
+	"encoding/json"
+)
+
+const url = "https://everyday-cc.github.io/ai/api/news/"
+
+type newsItem struct {
+	Page   int `json:"page"`
+   Value   []struct {
+	Path            string `json:"path"`
+	Title           string `json:"title"`
+	Excerpt         string `json:"excerpt"`
+	SourceURL       any    `json:"sourceUrl"`
+	WebURL          string `json:"webUrl"`
+	OriginalURL     string `json:"originalUrl"`
+	FeaturedContent any    `json:"featuredContent"`
+	Highlight       any    `json:"highlight"`
+	Heat            int    `json:"heat"`
+	Tags            any    `json:"tags"`
+	Images          []struct {
+		URL         string `json:"url"`
+		Width       int    `json:"width"`
+		Height      int    `json:"height"`
+		Title       any    `json:"title"`
+		Attribution any    `json:"attribution"`
+		IsCached    bool   `json:"isCached"`
+	} `json:"images"`
+	Content           string `json:"content"`
+	Type              string `json:"type"`
+	AmpWebURL         any    `json:"ampWebUrl"`
+	CdnAmpWebURL      any    `json:"cdnAmpWebUrl"`
+	PublishedDateTime string `json:"publishedDateTime"`
+	UpdatedDateTime   any    `json:"updatedDateTime"`
+	Provider          struct {
+		Name   string `json:"name"`
+		Domain string `json:"domain"`
+		Images []struct {
+			URL         string `json:"url"`
+			Width       int    `json:"width"`
+			Height      int    `json:"height"`
+			Title       any    `json:"title"`
+			Attribution any    `json:"attribution"`
+			IsCached    bool   `json:"isCached"`
+		} `json:"images"`
+		Publishers any `json:"publishers"`
+		Authors    any `json:"authors"`
+	} `json:"provider"`
+	Locale     string   `json:"locale"`
+	Categories []string `json:"categories"`
+	Topics     []string `json:"topics"`
+   }
+   NextPage int `json:"nextPage"`
+}
+
+func main() {
+
+	http.HandleFunc("/", aiNews)
+    	http.ListenAndServe(":8080", nil)
+
+}
+
+func aiNews ( w http.ResponseWriter, r *http.Request) {
+
+	var data newsItem
+	req, _ := http.NewRequest("GET", url, nil)
+
+	// Create Template
+	t, err := template.New("foo").Parse(`<table><th>AI News</th>{{range .}}<tr onMouseOver="this.style.backgroundColor='#F8F8F8'" onMouseOut="this.style.backgroundColor='#FFFFFF'"><td> <a href="{{.WebURL}}">{{.Title}}</a><br/><table><tr><td><span style="font-size:small">{{.Excerpt}}</span></br><span style="font-size:small"><a href="{{.Provider.Domain}}">{{.Provider.Name}}</a> | {{.Topics}} | {{.PublishedDateTime}} </span><br/></td></tr></table></td></tr>{{end}}</table>`)
+
+	// Create client
+        res, _ := http.DefaultClient.Do(req)
+        defer res.Body.Close()
+
+	// Get news data
+        body, _:= io.ReadAll(res.Body)
+	err = json.Unmarshal([]byte(body), &data)
+	if err != nil {
+		fmt.Printf("Could not unmarshal json: %s\n", err)
+		return
+	}
+
+	// Send data to stdout ( Debugging ) 
+	/*
+        fmt.Println(string(body))
+	var iter = 0
+	for _ , item := range data.Value{
+		fmt.Println("Title: " + item.Title)
+		fmt.Printf("URL: %v\n\n", item.WebURL)
+		iter++
+	}
+	*/
+
+	err = t.Execute(w, data.Value)
+
+}
+
